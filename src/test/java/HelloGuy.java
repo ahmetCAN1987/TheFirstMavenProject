f9d9c14c335f4011518423ad846f08b5865b804a
ahmetCAN1987 <exmetjan1987@gmail.com>
2021-12-27 17:46:28 +0300

    I have done the commit for maven files

--- src/test/java/HelloGuy.java
+++ src/test/java/HelloGuy.java
@@ -1,5 +1,6 @@
 public class HelloGuy {
     public static void main(String[] args) {
-        System.out.println("hellp testers...");
+        System.out.println("help testers...");
+        System.out.println("我的家乡在远方。。。");
     }
 }
