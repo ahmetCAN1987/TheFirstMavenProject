2b52c9620fed056295eabace255e1754d76a84ff
ahmetCAN1987 <exmetjan1987@gmail.com>
2021-12-27 18:01:56 +0300

    please allah help me,please

--- src/test/java/HelloGuy.java
+++ src/test/java/HelloGuy.java
@@ -2,5 +2,7 @@ public class HelloGuy {
     public static void main(String[] args) {
         System.out.println("help testers...");
         System.out.println("我的家乡在远方。。。");
+        System.out.println("local Repo is already published to github");
+        System.out.println("Doing more work for feature.");
     }
 }
